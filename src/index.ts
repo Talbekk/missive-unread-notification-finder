84b59ea74ec0411301bbba1bacd1dff72ba9d32d
talbekk <michael.campbell@gorilla.sc>
2026-06-26 18:44:37 +0100

    adds env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+import dotenv from 'dotenv';
+
+// Load .env file into process.env
+dotenv.config();
+
+// Access variables with full type support
+const apiKey: string | undefined = process.env.MISSIVE_API_KEY;
+
+if (!apiKey) {
+  throw new Error('Missing required environment variables');
+}
+
 export const getUnreadNotificationCount = async (): Promise<string> => {
     console.log(`hey`);
     return 'hello';
