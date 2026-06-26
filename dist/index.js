32c28e453353c7ab4ff2556f83632f9f42af2fe5
talbekk <michael.campbell@gorilla.sc>
2026-06-26 18:31:39 +0100

    setup project

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getUnreadNotificationCount = void 0;
+const getUnreadNotificationCount = async (apiKey) => {
+    return 'hello';
+};
+exports.getUnreadNotificationCount = getUnreadNotificationCount;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export const getUnreadNotificationCount = async (): Promise<string> => {
+    console.log(`hey`);
+    return 'hello';
+}
+
+getUnreadNotificationCount();
